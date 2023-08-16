8fbf32ba81bec4b598a6177aec142db45e632773
tagtart1 <adamtbizz@gmail.com>
2023-08-16 18:37:48 -0500

    Clean up project

---
+++ src/LogIn.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LogIn = () => {
+  return (
+    <section>
+      <input type="text" />
+      <input type="text" />
+    </section>
+  );
+};
+
+export default LogIn;
